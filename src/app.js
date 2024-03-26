a0697ecc0abb88196076dca32ad79fe55dba96f9
rociomr08 <rociominano08@gmail.com>
2024-03-26 12:16:37 -0500

    segundo cambio realizado corrección de paginas html,css y js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Usando JavaScript, cree una función que genere y devuelva una excusa aleatoria con la siguiente estructura:
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuseGenerator(){
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let numWho = Math.floor(Math.random() * who.length);
+  let numAction = Math.floor(Math.random() * action.length);
+  let numWhat = Math.floor(Math.random() * what.length);
+  let numWhen = Math.floor(Math.random() * when.length);
+  return who[numWho] + " " + action[numAction] + " " + what[numWhat] + " " + when[numWhen];
+}
+
+function onLoad(){
+  var excusa = document.querySelector('#excuse');
+  excusa.innerHTML = excuseGenerator();/*con el innerHTML remplazamos el texto del html por el contenido de la funcion excuseGenertor*/
+}
+window.onload = onLoad;
